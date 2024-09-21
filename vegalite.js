d16a75c3de1168d69f92dc0d2d68519914d47714
ecyl456 <eche0083@student.monash.edu>
2024-09-22 02:55:27 +0800

    Changed directory

--- vegalite.js
+++ vegalite.js
@@ -6,8 +6,3 @@ vegaEmbed("#choropleth_map", vg_1)  // Embeds the chart into an HTML element wit
         // You can access the Vega view instance as result.view
     })
     .catch(console.error);
-
-
-fetch('https://raw.githubusercontent.com/ecyl456/week9_homework/refs/heads/main/ne_1.json')
-    .then(response => response.json())
-    .then(data => console.log(data.objects));
